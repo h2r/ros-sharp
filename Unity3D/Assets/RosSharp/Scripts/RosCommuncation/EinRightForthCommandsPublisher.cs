ef2778e00cd2cde0bedf04fd1c7d0ea419845acc
David Whitney <david_whitney@brown.edu>
2018-04-22 20:24:29 -0400

    changed the einForthCommands script to experiment with VRTK controllers and sending messages to the correct ROS topic

--- Unity3D/Assets/RosSharp/Scripts/RosCommuncation/EinRightForthCommandsPublisher.cs
+++ Unity3D/Assets/RosSharp/Scripts/RosCommuncation/EinRightForthCommandsPublisher.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 
 public class EinRightForthCommandsPublisher : Publisher {
 
-    public string Topic = "\ein\right\forth_commands";
+    public string Topic = "ein/right/forth_commands";
+    private StandardString message;
+    private VRTK.VRTK_ControllerEvents controller;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rosSocket = GetComponent<RosConnector>().RosSocket;
 
-        publicationId = rosSocket.Advertise("/ein/right/forth_commands", "std_msgs/String");
-        PublicationEvent += ReadMessage;
+        // Get the controller component of this gameobject
+        controller = GetComponent<VRTK.VRTK_ControllerEvents>();
+
+        publicationId = rosSocket.Advertise("ein/right/forth_commands", "std_msgs/String");
+        message = new StandardString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (controller.gripPressed) {
+            message.data = "dogSummonSpot";
+            rosSocket.Publish(publicationId, message);
+            Debug.Log("button press");
+        }
+
+        message.data = "dogSummonSpot";
+        rosSocket.Publish(publicationId, message);
+
+
+    }
 }
