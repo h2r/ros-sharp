1f2fe393a2f6046fe4175c59291411356cbacad5
Nishanth J Kumar <nishanth.kumar20@gmail.com>
2018-07-06 17:49:10 -0400

    Seeing where persistent wav audio files are written

---
+++ Unity3D/Assets/RosSharp/Scripts/RosCommuncation/AudioPublisher.cs
@@ -0,0 +1,38 @@
+﻿using RosSharp.RosBridgeClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+// The class for this script extends the Publisher class of ROS# which was authored by Dr. Martin Bischoff
+public class AudioPublisher : Publisher
+{
+
+    private SensorAudio message;
+    private AudioClip audioRecording;
+    private String mic;
+
+    // Used for initialization
+    protected override void Start()
+    {
+        rosSocket = GetComponent<RosConnector>().RosSocket;
+        publicationId = rosSocket.Advertise(Topic, "audio_common_msgs/AudioData");
+        message = new SensorAudio();
+        InvokeRepeating("SendAudio", 1.5f, 1.5f);
+    }
+
+    void SendAudio()
+    {
+        mic = Microphone.devices[0];
+        audioRecording = Microphone.Start(mic, false,1,16000);
+        message.data = WavUtility.FromAudioClip(audioRecording);
+
+        if (message.data != null)
+        {
+            Debug.Log(message.data[2]);
+            rosSocket.Publish(publicationId, message);
+        }
+    }
+
+}
